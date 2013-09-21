9c0b64d7520ce32e9073e5a71fc6349cad87eebc
Mariusz Kryński <mrk@sed.pl>
2013-09-21 19:10:04 +0200

    bugfix

--- azurite/storage.py
+++ azurite/storage.py
@@ -83,7 +83,7 @@ class AzureStorage(Storage):
         if hasattr(content.file, 'content_type'):
             content_type = content.file.content_type
         else:
-            content_type = mimetypes.guess_type(name)
+            content_type, _ = mimetypes.guess_type(file_path)
 
         if hasattr(content, 'chunks'):
             content_str = ''.join(chunk for chunk in content.chunks())
